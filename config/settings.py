d8443e6f6f608a252f4987c78a8f583a24f87568
Varun Kumar <iamvarunkumar23@gmail.com>
2025-03-28 23:19:26 +0530

    feat: Add basic CSS styling for gallery and detail pages

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Add this: Tells Django to look in a 'static' folder in the project root
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
